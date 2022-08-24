3155af93ae9609a4b11aa072b7ae2ac5abdf95b0
LooseLi <1329307562@qq.com>
2022-08-24 17:48:44 +0800

    :pencil: setup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,27 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, defineProps } from 'vue'
 
-defineProps<{ msg: string }>()
+const props = defineProps({
+  msg: {
+    type: String,
+    default: 'test'
+  }
+})
 
-const count = ref(0)
+const count = ref(100)
+
+const add = () => {
+  console.log(props.msg)
+  count.value++
+}
+
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="add">count is {{ count }}</button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
