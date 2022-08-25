c28baceecd0399c4ca837e4d5ebd4e87b3856188
LooseLi <1329307562@qq.com>
2022-08-25 17:11:17 +0800

    :pencil: alias

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,24 @@
 <template>
   <h1>首页</h1>
   <p>{{ $store.state.count }}</p>
+  <img src="@/assets/vue.svg" alt="">
+  <div class="box"></div>
 </template>
 
 <script lang="ts" setup>
 import { useStore } from 'vuex'
+import user from '@/api/user'
 
 const store = useStore()
-
 console.log(store.state.count)
+
+console.log(user)
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.box {
+  width: 20px;
+  height: 20px;
+  background: url("@/assets/vue.svg") no-repeat;
+}
+</style>
